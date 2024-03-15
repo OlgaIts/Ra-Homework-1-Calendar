1ab7c1f997db953025540c73e8ebd1e3d7fc7b31
Olga Its <olga-tissen@tutanota.com>
2024-03-15 22:45:53 +0100

    доработала компонент daysgrid

--- src/components/DaysGrid/DaysGrid.jsx
+++ src/components/DaysGrid/DaysGrid.jsx
@@ -6,31 +6,20 @@ export const DaysGrid = ({startDay}) => {
   const day = startDay.clone().subtract(1, "day");
   const daysArray = [...Array(42)].map(() => day.add(1, "day").clone());
   const currentDay = moment().format("MMMM Do YYYY");
-
-  // const startOfMonth = moment().startOf("month");
-  // const endOfMonth = moment().endOf("month");
-
-  // const currentMonth = moment().month(2).startOf("month");
-  // const monthDays = [...Array(31)].map(() =>
-  //   currentMonth.add(1, "day").clone(),   Начинается со 2 марта, а должен с первого...
-  // );       
-
-  // console.log(monthDays); Но даже если бы получилось, все равно не знаю как это применить
+  const isCurrentMonth = (date) => moment().month() === moment(date).month();
 
   return (
     <div className={styles.grid}>
-      {daysArray.map((item) => {
-        return (
-          <div
-            className={`${styles.cellWrapp} ${
-              item.format("MMMM Do YYYY") === currentDay ? styles.active : ""
-            } `}
-            key={item}
-          >
-            {item.format("D")}
-          </div>
-        );
-      })}
+      {daysArray.map((item) => (
+        <div
+          className={`${styles.cellWrapp} ${
+            item.format("MMMM Do YYYY") === currentDay ? styles.active : ""
+          } ${isCurrentMonth(item) ? styles.other : ""}`}
+          key={item}
+        >
+          {item.format("D")}
+        </div>
+      ))}
     </div>
   );
 };
